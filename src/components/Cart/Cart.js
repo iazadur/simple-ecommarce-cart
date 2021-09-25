dcef4f1efc4c733f8385b8e4dc56571889f85a31
iazadur <iamazadur@gmail.com>
2021-09-25 08:39:29 +0600

    finished commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const Cart = (props) => {
-    const {cart} = props || []
-    console.log(cart.length);
+    const { cart } = props || {}
+    // using reduce 
+    const totalReducer = (prevValue, currentValue) => prevValue + currentValue.price;
+    const total = cart.reduce(totalReducer, 0)
     return (
         <div>
-            <h3><i className="fas fa-shopping-cart"></i> md azadur rahman</h3>
+            <h3><i className="fas fa-shopping-cart"></i> Shopping Cart</h3>
             <h4>items orders: {cart.length}</h4>
+            <h4>items orders: {total}</h4>
+
+            <ul>
+                {
+                    cart.map(product => <li>{product.name}</li>)
+                }
+            </ul>
 
         </div>
     );
